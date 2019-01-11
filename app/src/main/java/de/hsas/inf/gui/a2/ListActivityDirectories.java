2a4035d89a6b81fd80d170888f09a3ef075c2022
Jolare <jolare.ky@gmail.com>
2019-01-11 01:22:09 +0100

    first commit, Gesten und Gallerie fehlen noch

---
+++ app/src/main/java/de/hsas/inf/gui/a2/ListActivityDirectories.java
@@ -0,0 +1,148 @@
+package de.hsas.inf.gui.a2;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class ListActivityDirectories extends AppCompatActivity {
+
+    private final String SCREENSHOT_DIR = "Screenshots";
+    private final String CAMERA_DIR = "Camera";
+    private final String[] perms = {"android.permission.READ_EXTERNAL_STORAGE", "android.permission.WRITE_EXTERNAL_STORAGE"};
+    private String selectedDirectory="";
+    private String [] titles = {SCREENSHOT_DIR, CAMERA_DIR, "Choose other directory..."};
+    // Wird spaeter gebraucht
+    private String pathScreenShots =
+      Environment.getExternalStoragePublicDirectory(
+        Environment.DIRECTORY_PICTURES).getAbsolutePath();
+    private String pathCamera =
+      Environment.getExternalStoragePublicDirectory(
+        Environment.DIRECTORY_DCIM).getAbsolutePath() + "Camera";
+
+    private ListView list;
+    private Button btn_auswahl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_directories);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        list = findViewById(android.R.id.list);
+        btn_auswahl = findViewById(R.id.btn_auswahl);
+        btn_auswahl.setEnabled(false);
+        // Auswahlmoeglichkeiten (hier nur 2) in ArrayAdapter ...
+        ArrayAdapter<String> myList =
+          new ArrayAdapter<String>(this,R.layout.list_content,
+            titles);
+        // ArrayAdapter mit ListView verknuepfen
+        list.setAdapter(myList);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String directory = (String)parent.getItemAtPosition(position);
+                switch (directory){
+                    case "Screenshots":
+                        selectedDirectory = pathScreenShots;
+                        btn_auswahl.setEnabled(true);
+                        break;
+                    case "Camera":
+                        selectedDirectory = pathCamera;
+                        btn_auswahl.setEnabled(true);
+                        break;
+                    default:
+                        btn_auswahl.setEnabled(false);
+                }
+            }
+        });
+        if(checkPermissions() == false) {
+            try {
+                ActivityCompat.requestPermissions((Activity) ListActivityDirectories.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 3);
+            } catch (Exception e){
+                e.printStackTrace();
+                throw e;
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_list_activity_directories, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void startListOnClick(){
+        list = findViewById(android.R.id.list);
+        String listPath = (String) list.getSelectedItem();
+        switch (listPath){
+            case "Screenshots":
+                selectedDirectory = pathScreenShots;
+                break;
+            case "Camera":
+                selectedDirectory = pathCamera;
+                break;
+        }
+    }
+
+    public void startImageActivity(View view) {
+        Intent startnext;
+        startnext = new Intent(ListActivityDirectories.this,
+          ShowImage.class);
+        // PfadInfo an SubActivity weiterleiten
+        // Hier geben Sie spaeter den tatsaechlich ausgewaehlten Pfad
+        // an!!
+        // Bundle enthaelt die zu uebergebenden Infos ...
+        Bundle infos = new Bundle();
+        // ... nach put...
+        infos.putStringArray("PATH_INFOS",titles);
+        infos.putString("SELECTED_DIRECTORY",selectedDirectory);
+        startnext.putExtras(infos);
+        // Activity starten mit Uebergabe der Infos ...
+        this.startActivity(startnext);
+    }
+
+    private boolean checkPermissions() {
+        boolean result = true;
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int granted = ContextCompat.checkSelfPermission(ListActivityDirectories.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+            if(granted != PackageManager.PERMISSION_GRANTED) {
+                result = false;
+            }
+            // granted = getApplicationContext().checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        return result;
+    }
+}
---
+++ app/src/main/java/de/hsas/inf/gui/a2/ShowImage.java
@@ -0,0 +1,30 @@
+package de.hsas.inf.gui.a2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ShowImage extends AppCompatActivity {
+
+  private TextView pfade;
+  private ImageView image; // Von Ihnen weiter einzubinden
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_show_image);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    pfade = findViewById(R.id.showPath);
+    String [] pfadeStrings =
+      getIntent().getExtras().getStringArray("PATH_INFOS");
+    String directory = getIntent().getExtras().getString("SELECTED_DIRECTORY");
+    pfade.setText(pfadeStrings[0] + "\n" + pfadeStrings[1] + "\n" +  directory);
+  }
+
+}
